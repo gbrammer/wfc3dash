25c3c577ed035fe990b4232e535bb86dd06d388d
Gabriel Brammer <brammer@stsci.edu>
2018-06-04 10:08:43 -0400

    Update demo script

--- wfc3dash/demo.py
+++ wfc3dash/demo.py
@@ -40,7 +40,8 @@ for file in ima_files:
     visits.append(visit)
 
 # Or use grizli to generate the exposure-level visits
-visits, _, _ = auto_script.parse_visits(field_root='dash', HOME_PATH='/tmp/', is_dash=True)
+visits, _, _ = auto_script.parse_visits(field_root='dash', HOME_PATH='/tmp/',
+                                        is_dash=True)
 
 # Manual alignment, shifts can be very large
 ds9 = grizli.ds9.DS9()
@@ -58,5 +59,9 @@ radec = 'ipac_acs_galaxy.radec'
 #     - Fill centers of saturated stars with ePSF model
 #     - Visit-level catalog & combined image
 for visit in visits:
-    status = prep.process_direct_grism_visit(direct=visit, grism={}, radec=radec, skip_direct=False, align_mag_limits=[14,24], tweak_max_dist=8, tweak_threshold=8, align_tolerance=8, tweak_fit_order=2)
+    status = prep.process_direct_grism_visit(direct=visit, grism={},
+                                    radec=radec, skip_direct=False,
+                                    align_mag_limits=[14,24], 
+                                    tweak_max_dist=8, tweak_threshold=8, 
+                                    align_tolerance=8, tweak_fit_order=2)
 
