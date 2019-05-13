aafdd175e229a5c7bf9ac1acd62c58b57ddd83ea
Gabriel Brammer <gbrammer@gmail.com>
2019-05-13 16:07:18 +0200

    Use setuptools and add version

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
+
+version = "0.1" # First version
+
+version_str = """# 
+__version__ = "{0}"\n""".format(version)
+
+fp = open('wfc3dash/version.py','w')
+fp.write(version_str)
+fp.close()
 
 setup(name='wfc3dash',
-      version='0.1',
+      version=version,
       description='Helper scripts for WFC3 DASH observations',
       author='Gabriel Brammer',
-      author_email='brammer@stsci.edu',
+      author_email='gbrammer@gmail.com',
       url='https://github.com/gbrammer/wfc3dash/',
       packages=['wfc3dash'],
      )
