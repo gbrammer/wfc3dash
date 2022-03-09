d9cfdac1049055433cc2d1b1647adebaa783ae38
Gabriel Brammer <gbrammer@gmail.com>
2022-03-09 12:47:01 +0100

    add grism files

---
+++ wfc3dash/grism/alignment.py
@@ -0,0 +1,349 @@
+import astropy.io.fits as pyfits
+from skimage.feature import peak_local_max
+from skimage.feature import match_template
+
+from grizli import multifit
+spl = utils.bspline_templates(wave=np.arange(8000, 1.8e4, 10), df=3)
+
+import scipy.ndimage as nd
+import golfir.utils
+from grizli import prep
+from scipy.optimize import minimize
+
+def add_ytrace_offset(self, yoffset):
+    """Add an offset in Y to the spectral trace
+
+    Parameters
+    ----------
+    yoffset : float
+        Y-offset to apply
+
+    """
+    from grizli.utils_c import interp
+    
+    self.ytrace_beam, self.lam_beam = self.conf.get_beam_trace(
+                        x=(self.xc+self.xcenter-self.pad)/self.grow,
+                        y=(self.yc+self.ycenter-self.pad)/self.grow,
+                        dx=(self.dx+self.xcenter*0+self.xoff)/self.grow,
+                        beam=self.beam, fwcpos=self.fwcpos)
+
+    self.ytrace_beam *= self.grow
+    self.yoffset = yoffset
+
+    self.ytrace_beam += yoffset
+
+    # Integer trace
+    # Add/subtract 20 for handling int of small negative numbers
+    dyc = np.cast[int](self.ytrace_beam+20)-20+1
+
+    # Account for pixel centering of the trace
+    self.yfrac_beam = self.ytrace_beam - np.floor(self.ytrace_beam)
+
+    try:
+        self.flat_index = self.idx[dyc + self.x0[0], self.dxpix]
+    except IndexError:
+        # print 'Index Error', id, self.x0[0], self.xc, self.yc, self.beam, self.ytrace_beam.max(), self.ytrace_beam.min()
+        raise IndexError
+
+    # Trace, wavelength, sensitivity across entire 2D array
+    self.ytrace, self.lam = self.conf.get_beam_trace(
+                        x=(self.xc+self.xcenter-self.pad)/self.grow,
+                        y=(self.yc+self.ycenter-self.pad)/self.grow,
+                        dx=(self.dxfull+self.xcenter+self.xoff)/self.grow,
+                        beam=self.beam, fwcpos=self.fwcpos)
+
+    self.ytrace *= self.grow
+    self.ytrace += yoffset
+
+    # Reset sensitivity
+    ysens = self.lam_beam*0
+    so = np.argsort(self.lam_beam)
+
+    conf_sens = self.conf.sens[self.beam]
+    if self.MW_F99 is not None:
+        MWext = 10**(-0.4*(self.MW_F99(conf_sens['WAVELENGTH']*u.AA)))
+    else:
+        MWext = 1.
+
+    ysens[so] = interp.interp_conserve_c(self.lam_beam[so],
+                                         conf_sens['WAVELENGTH'],
+                                         conf_sens['SENSITIVITY']*MWext,
+                                         integrate=1, left=0, right=0)
+    self.lam_sort = so
+    self.sensitivity_beam = ysens
+
+    # Full array
+    ysens = self.lam*0
+    so = np.argsort(self.lam)
+    ysens[so] = interp.interp_conserve_c(self.lam[so],
+                                         conf_sens['WAVELENGTH'],
+                                         conf_sens['SENSITIVITY']*MWext,
+                                         integrate=1, left=0, right=0)
+
+    self.sensitivity = ysens
+
+
+#
+def get_beam_kernel(file):
+    """
+    Kernel for source identification
+    """
+    from grizli import model
+    
+    # Kernel for matching
+    flt = model.GrismFLT(grism_file=file, sci_extn=1, pad=0)
+    size = 16
+    cut = flt.compute_model_orders(x=507, y=507, size=16, get_beams='A', 
+                                   in_place=False) #, psf_params=[0,0])
+    
+    beam = model.BeamCutout(flt=flt, beam=cut['A'], conf=flt.conf)
+    beam.init_epsf(psf_params=[0,0], get_extended=True)
+    
+    beam.psf_params = np.array([0,0])
+
+    xspec = np.arange(5000,2.e4)
+    yspec = (xspec/1.3e4)**-1.
+
+    beam.compute_model(use_psf=True, spectrum_1d=(xspec, yspec*1.e-19), is_cgs=True)
+    kernel = beam.model*1.#/beam.model.sum()
+
+    for r in [-2,-1,1,2]:
+        kernel += np.roll(beam.model, r, axis=0)*2.**-np.abs(r)
+
+    kernel /= kernel.sum()
+
+    k0 = kernel*1
+    kx = kernel*1
+    kx[:, np.abs(beam.wave-1.39e4) < 2300] = 0
+
+    plt.imshow(kernel)
+
+    beam.sh
+
+    conf = beam.beam.conf    
+    return beam, conf, kernel, kx, k0
+
+
+def align_dash_exposure(flt_file='iehn5vr8a_flt.fits', verbose=0):
+    """
+    """
+    
+    # Detect grism "objects"
+    im = pyfits.open(flt_file)
+    
+    utils.unset_dq_bits(im['DQ'].data, 512)
+
+    sci = im['SCI'].data*1
+    dq = im['DQ'].data > 0
+    var = im['ERR'].data**2
+    ivar = 1/var
+    #ivar = ivar*0.+np.median(ivar[~dq])
+    dq |= ~np.isfinite(sci+ivar)
+
+    # quick CRs
+    fthresh=0.2
+    kern = np.zeros((101,21))
+    kern[50,10] = 1
+    filt = match_template(sci, kern.T, pad_input=True)
+
+    sat = filt > fthresh
+    nsat = 4
+
+    for g in range(nsat):
+        sat = nd.binary_dilation(sat) & (sci > 7*im['ERR'].data)
+
+    dq |= sat
+
+    ivar[dq] = 0
+
+    sky = np.median(sci[~dq])
+    
+    beam, conf, kernel, kx, k0 = get_beam_kernel(flt_file)
+    
+    cnum = golfir.utils.convolve_helper((sci-sky)*ivar, kernel)
+    cden = golfir.utils.convolve_helper(ivar, kernel**2)
+    cden2 = golfir.utils.convolve_helper(ivar, kernel)
+
+    cnumx = golfir.utils.convolve_helper((sci-sky)*ivar, kx)
+    cdenx = golfir.utils.convolve_helper(ivar, kx**2)
+
+    flt_sn = cnum*np.sqrt(cden)
+    nb_labels = 0
+    threshold = 100
+
+    while nb_labels < 16:
+        threshold *= 0.9
+        label_objects, nb_labels = nd.label(flt_sn > threshold)
+
+    #print(threshold, nb_labels)
+
+    fp = k0 > 0.001*k0.max()
+
+    yc, xc = peak_local_max(flt_sn, footprint=fp*1, threshold_abs=threshold, 
+                            labels=label_objects, 
+                            num_peaks_per_label=1).T
+
+    clip = (xc > 100) & (xc < 1014-100)
+    xc = xc[clip]
+    yc = yc[clip]
+    
+    # Make figure
+    src_fig, ax = plt.subplots(1,1,figsize=(10,10))
+    ax.imshow(label_objects, cmap='rainbow')
+    ax.scatter(xc, yc, color='r', marker='x')
+    src_fig.savefig(flt_file.split('_flt')[0]+'.src.png')
+    
+    # project grism positions based on trace
+
+    #conf = beam.beam.conf
+    x0 = 103
+    dy0, lam0 = conf.get_beam_trace(xc, yc, x0)
+    dy1, lam1 = conf.get_beam_trace(xc, yc, x0+1)
+
+    dy, lam_ref = conf.get_beam_trace(507, 507, x0)
+
+    dlam = lam1-lam0
+    dx = -(lam0-lam_ref)/dlam + x0
+
+    import astropy.wcs 
+    wcs = astropy.wcs.WCS(im['SCI'].header, relax=True)
+    xd, yd = xc-dx, yc-dy0-dy
+    
+    has_shift = xd > 1e10
+    
+    #####################
+    # Refine positions
+    pad = 100
+    flt = model.GrismFLT(grism_file=flt_file, sci_extn=1, pad=pad)
+    
+    for i in range(len(xd)):
+        size = 16
+
+        print(i, xd[i]+pad, yd[i]+pad)
+        #ds9.set(f'pan to {xd[i]+pad+1} {yd[i]+pad+1}')
+
+        cut = flt.compute_model_orders(x=xd[i]+pad, y=yd[i]+pad, size=size,
+                                       get_beams='A', in_place=False)
+        beam = model.BeamCutout(flt=flt, beam=cut['A'], conf=flt.conf, 
+                                min_sens=0.001, min_mask=0.001)
+
+        # centered PSF as reference
+        beam.beam.total_flux = 1
+        beam.init_epsf(psf_params=[0,0], get_extended=True)
+        psf = np.abs(beam.beam.psf)*1
+
+        beam = model.BeamCutout(flt=flt, beam=cut['A'], conf=flt.conf, min_sens=0.001, min_mask=0.001)
+        beam.beam.direct = psf.astype(np.float32)*1
+        beam.beam.total_flux = beam.beam.direct.sum()
+        beam.beam.seg = (psf > 0.01*psf.max()).astype(np.float32)*(i+1)
+
+        beam.id = beam.beam.id = int(beam.beam.seg.max())
+        beam.beam.set_segmentation(beam.beam.seg)
+
+        beam._parse_from_data(**beam._parse_params)
+        beam.compute_model()
+        
+        ###### Multibeam
+        mb = multifit.MultiBeam([beam], root='test', min_sens=-1, min_mask=-1, mask_resid=False, fcontam=0)
+
+        # Initial chi-squared
+        tfit = mb.template_at_z(templates=spl)
+        chi0 = tfit['chi2']
+
+        # Grow fit mask
+        for b in mb.beams:
+            b._parse_from_data(**b._parse_params)
+            fm = nd.binary_dilation(b.fit_mask.reshape(b.sh), iterations=5)
+            b.fit_mask = fm.flatten() & (b.ivarf > 0)
+
+        mb.compute_model()
+
+        mb._parse_beam_arrays()
+        mb.initialize_masked_arrays()
+
+        def objfun_sens(shift):
+            #print(shift)
+
+            self = mb.beams[0].beam
+            self.xoff = shift[0]
+            add_ytrace_offset(self, shift[1])
+
+            tfit = mb.template_at_z(templates=spl)
+            if verbose > 1:
+                print(shift, tfit['chi2'])
+            
+            return tfit['chi2']
+
+        # First fit
+        _x = minimize(objfun_sens, np.zeros(2), bounds=((-5,5), (-5, 5)), method='powell')
+        
+        # Reset fit mask
+        for b in mb.beams:
+            # b._parse_from_data()
+            b._parse_from_data(**b._parse_params)
+
+            # Needed for background modeling
+            if hasattr(b, 'xp'):
+                delattr(b, 'xp')
+
+        mb._parse_beam_arrays()
+        mb.initialize_masked_arrays()
+        mb.compute_model()
+        
+        # Refit
+        _x = minimize(objfun_sens, np.zeros(2), bounds=((-5,5), (-5, 5)), method='powell')
+        
+        # Reset fit mask
+        for b in mb.beams:
+            # b._parse_from_data()
+            b._parse_from_data(**b._parse_params)
+
+            # Needed for background modeling
+            if hasattr(b, 'xp'):
+                delattr(b, 'xp')
+
+        mb._parse_beam_arrays()
+        mb.initialize_masked_arrays()
+        mb.compute_model()
+        
+        tfit = mb.template_at_z(templates=spl)
+        chi_final = tfit['chi2']
+
+        shift = _x.x
+
+        if (chi_final - chi0 < -50) & (np.abs(shift).max() < 4.8):
+            #fig = mb.oned_figure(tfit=tfit, units='eps') #, trace_limits=[-2,2])
+            xd[i] += shift[0]
+            yd[i] += shift[1]
+            has_shift[i] = True
+            print(' apply shift:',i, shift, chi_final - chi0)
+        else:
+            print('  -    shift:',i, shift, chi_final - chi0)
+    
+    tab = utils.GTable()
+    tab['ra'], tab['dec'] = wcs.all_pix2world(xd, yd, 0)
+    tab['xd'], tab['yd'] = xd, yd
+    tab['has_shift'] = has_shift
+    
+    return tab
+
+if 0:
+    import glob
+    import os
+    os.chdir('/mnt/efs/fs1/telescopes/notebooks/Dash/j100232p0247/Prep')
+
+    files = glob.glob('iehn5vrg[a-o]_flt.fits')
+
+    os.chdir('/mnt/efs/fs1/telescopes/notebooks/Dash/j100232p0243/Prep')
+
+    files = glob.glob('iehn5wb1[a-o]_flt.fits')
+    # files = glob.glob('iehn5wba[a-o]_flt.fits')
+
+    #files = glob.glob('iehn5vr8[a-o]_flt.fits')
+    files.sort()
+    tabs = {}
+    for file in files:
+        print(f'============\n{file}\n============')
+        if file in tabs:
+            continue
+        tabs[file] = align_dash_exposure(flt_file=file, verbose=0)
---
+++ wfc3dash/grism/grism.py
@@ -0,0 +1,279 @@
+"""
+Process DASH grism files from 
+"""
+
+import os
+import glob
+
+import numpy as np
+import scipy.ndimage as nd
+
+import matplotlib.pyplot as plt
+plt.rcParams['image.origin'] = 'lower'
+plt.rcParams['image.interpolation'] = 'Nearest'
+
+#HOME_PATH = '/home/ec2-user/telescopes/notebooks/Dash'
+HOME_PATH = '/GrizliImaging/'
+
+os.chdir(HOME_PATH)
+
+import astropy.wcs as pywcs
+import astropy.io.fits as pyfits
+import astropy.units as u
+
+import grizli
+from grizli import utils, model, multifit, prep
+from grizli.pipeline import auto_script
+from grizli.aws import db, visit_processor
+
+import golfir.utils
+
+PATHS = None
+
+def process_association(assoc='j100028p0215_0619_ehn_cosmos-g141-101_wfc3ir_g141', clean=True, align_kwargs={}, **kwargs):
+    """
+    Process a DASH grism association
+    """
+    from stwcs import updatewcs    
+    from wfc3dash import process_raw
+    
+    global PATHS
+    
+    os.chdir(HOME_PATH)
+    
+    os.environ['orig_iref'] = os.environ.get('iref')
+    os.environ['orig_jref'] = os.environ.get('jref')
+    visit_processor.set_private_iref(assoc)
+    
+    visit_processor.update_assoc_status(assoc, status=1)
+    
+    tab = db.SQL(f"""SELECT * FROM assoc_table
+                     WHERE assoc_name='{assoc}'""")
+    
+    if len(tab) == 0:
+        print(f"assoc_name='{assoc}' not found in assoc_table")
+        return False
+        
+    keep = []
+    for c in tab.colnames:
+        if tab[c].dtype not in [object]:
+            keep.append(c)
+    
+    tab[keep].write(f'{assoc}_footprint.fits', overwrite=True)
+    
+    PATHS = auto_script.create_path_dict(assoc)
+
+    auto_script.fetch_files(assoc, s3_sync=True, reprocess_clean_darks=False, 
+                            reprocess_parallel=False)
+    
+    # DASHify the exposures
+    os.chdir(PATHS['raw'])
+    process_raw.run_all()
+    
+    # FLT copies
+    os.chdir(PATHS['prep'])
+    files = glob.glob(f'../RAW/*[a-o]_flt.fits')
+    files.sort()
+    for rfile in files:
+        file = os.path.basename(rfile)
+        prep.fresh_flt_file(file)
+        updatewcs.updatewcs(os.path.basename(file), verbose=False, 
+                            use_db=False)
+    
+    visits = []
+    qfiles = glob.glob('../RAW/*q_flt.fits')
+    qfiles.sort()
+    
+    for f in qfiles:
+        product = os.path.basename(f).split('q_flt.fits')[0]
+        visit = {'files':glob.glob(f'{product}[a-o]_flt.fits'), 
+                 'product':product}
+        visit['files'].sort()
+        visits.append(visit)
+    
+    for visit in visits:
+        if not os.path.exists(f"{visit['product']}_column.png"):
+            prep.visit_grism_sky(grism=visit)
+
+    for visit in visits:
+        align_visit(visit, **align_kwargs)
+    
+    finish_group(assoc, **kwargs)
+    
+def align_visit(visit, flag_crs=True, driz_cr_kwargs={'driz_cr_snr_grow':3}, **kwargs):
+    """
+    """
+    from skimage.transform import SimilarityTransform
+    
+    from tristars import match
+    from .alignment import align_dash_exposure
+
+    global PATHS
+    
+
+    os.chdir(PATHS['prep'])
+    ref_file = os.path.join(HOME_PATH, 
+                            'uvista_DR4_Ks_ADP.2019-02-05T17_00_54.618.fits')
+    eso_file = 'https://dataportal.eso.org/dataPortal/file/'
+    eso_file += 'ADP.2019-02-05T17:00:54.618'
+    if not os.path.exists(ref_file):
+        os.system(f'wget {eso_file} -O {ref_file}')
+
+    kcat = utils.read_catalog(ref_file)
+    kcat['H_AUTO'] = kcat['MAG_AUTO']
+    
+    tabs = {}
+    for file in visit['files']:
+        print(f'============\n{file}\n============')
+        if file in tabs:
+            continue
+        tabs[file] = align_dash_exposure(flt_file=file, verbose=0)
+        
+    plt.close("all")
+    
+    LOGFILE = visit['product']+'.log.txt'
+
+    # WCS alignment
+    msg = f'# file rmsx rmsy dx_arcsec dy_arcsec'
+    utils.log_comment(LOGFILE, msg, verbose=True, show_date=True)
+    
+    for k in tabs:
+        C1 = np.array([tabs[k]['ra'], tabs[k]['dec']]).T
+
+        idx, dr = tabs[k].match_to_catalog_sky(kcat)
+
+        mlim = 20.2 if len(kcat) > 2000 else 21.5
+
+        try:
+            clip = (dr < 2*u.arcmin) & (kcat['H_AUTO'] < mlim)
+        except:
+            clip = (dr < 1*u.arcmin) & (kcat['MAG_AUTO'] < 20.2)
+
+        C2 = np.array([kcat['ALPHA_J2000'][clip], 
+                       kcat['DELTA_J2000'][clip]]).T
+
+        x0 = np.mean(C1, axis=0)
+        V1 = (C1-x0)*3600
+        V2 = (C2-x0)*3600
+
+        pair_ix = match.match_catalog_tri(V1, V2, ignore_rot=True, 
+                                          ignore_scale=True,
+                                          size_limit=[1,110], 
+                                          auto_keep=False)
+
+        tfo, dx, rms = match.get_transform(V1, V2, pair_ix,
+                                           transform=SimilarityTransform, 
+                                           use_ransac=True)
+
+        ok = np.abs(dx.max(axis=1)) < 1
+
+        tfo, dx, rms = match.get_transform(V1, V2, pair_ix[ok,:], 
+                                           transform=SimilarityTransform, 
+                                           use_ransac=True)
+
+        fig = match.match_diagnostic_plot(V1, V2, pair_ix[ok,:], tf=tfo,
+                                                  new_figure=True)
+        
+        fig.axes[0].set_xlim(-180, 180)
+        fig.axes[0].set_ylim(-180, 180)
+        
+        fig.savefig(k.split('_flt')[0] + '.wcs.png')
+        
+        #print(k, rms, tfo.translation)
+        tr = tfo.translation
+        
+        msg = f'{k} {rms[0]:.2f} {rms[1]:.2f} {tr[0]:.2f} {tr[1]:.2f}'
+        utils.log_comment(LOGFILE, msg, verbose=True, show_date=False)
+        
+        im = pyfits.open(k, mode='update')
+        wcs = pywcs.WCS(im[1].header)
+        cosd = np.array([np.cos(wcs.wcs.crval[1]/180*np.pi), 1])
+        ddeg = tfo.translation/3600 * cosd
+        wcs.wcs.crval += ddeg
+
+        h = utils.to_header(wcs)
+        for hk in h:
+            if hk in im[1].header:
+                im[1].header[hk] = h[hk]
+        
+        if 'WCS_DX' in im[0].header:
+            im[0].header['WCS_DX'] += tfo.translation[0]
+            im[0].header['WCS_DY'] += tfo.translation[1]
+        else:
+            im[0].header['WCS_DX'] = tfo.translation[0], 'X WCS shift, arcsec'
+            im[0].header['WCS_DY'] = tfo.translation[1], 'Y WCS shift, arcsec'
+            
+        im.flush() #writeto('x'+im.filename(), overwrite=True)
+    
+    plt.close('all')
+    
+    if flag_crs:
+        prep.drizzle_overlaps([visit], parse_visits=False, 
+                              check_overlaps=False, scale=0.150, 
+                              pixfrac=1, final_kernel='point',
+                              run_driz_cr=True, skysub=False, 
+                              reset_bits=4096,
+                              **driz_cr_kwargs)
+
+
+def finish_group(assoc, filters=['F814W','F105W','F140W','F125W','F160W'], phot_kwargs={}, **kwargs):
+    """
+    Things to do as a group
+    """
+    
+    global PATHS
+    
+    os.chdir(os.path.join(HOME_PATH, assoc, 'Prep'))
+    
+    grism_files = glob.glob('iehn*flt.fits')
+    grism_files.sort()
+    
+    _h, ir_wcs = utils.make_maximal_wcs(grism_files, pixel_scale=0.1, 
+                                        get_hdu=False, pad=64)
+                                        
+    #### Reference mosaic
+    root = assoc    
+    visit_processor.cutout_mosaic(rootname=root, 
+                              skip_existing=False,
+                              ir_wcs=ir_wcs,
+                              filters=filters, 
+                              kernel='square', s3output=None, 
+                              gzip_output=False, clean_flt=True)
+    
+    golfir.catalog.make_charge_detection(root, ext='ir')
+    
+    auto_script.multiband_catalog(field_root=root, **phot_kwargs)
+    
+    grp = auto_script.grism_prep(field_root=root, 
+                                 gris_ref_filters={'G141':['ir']},
+                                 files=grism_files,
+                                 refine_mag_limits=[18,23])
+    
+    if len(glob.glob(f'{root}*_grism*fits*')) == 0:
+        grism_files = glob.glob('*GrismFLT.fits')
+        grism_files.sort()
+
+    # Drizzled grp objects
+    # All files
+    if len(glob.glob(f'{root}*_grism*fits*')) == 0:
+        grism_files = glob.glob('*GrismFLT.fits')
+        grism_files.sort()
+
+        catalog = glob.glob(f'{root}-*.cat.fits')[0]
+        try:
+            seg_file = glob.glob(f'{root}-*_seg.fits')[0]
+        except:
+            seg_file = None
+
+        grp = multifit.GroupFLT(grism_files=grism_files, direct_files=[], 
+                                ref_file=None, seg_file=seg_file, 
+                                catalog=catalog, cpu_count=-1, sci_extn=1, 
+                                pad=256)
+
+        # Make drizzle model images
+        grp.drizzle_grism_models(root=root, kernel='point', scale=0.15)
+
+        # Free grp object
+        del(grp)
+    
+    
